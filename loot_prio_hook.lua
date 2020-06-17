02e1cddbb0d87fa35dd1f0f30ec6a095ed9eb8e9
venuatu <venuatu@gmail.com>
2020-06-17 22:57:03 +1000

    add gp tooltips and printing based on alt item clicks

--- loot_prio_hook.lua
+++ loot_prio_hook.lua
@@ -6,39 +6,36 @@ local LOOT_ROLES = {
 	{"os", "os1: %s", 'main'},
 	{"open_ms1", "ms1: %s", 'open'},
 	{"open_ms2", "ms2: %s", 'open'},
+	{"ogp", "old: %s", 'gp'},
+	{"gp", "new: %s", 'gp'},
 }
 
-local TYPE_PREFIXES = {
-	["main"] = "|c00ff8000Syn Main: %s",
-	["open"] = "|c000070ddSyn Open: %s",
+local TYPE_TOOLTIP_PREFIXES = {
+	["main"] = "|c00ff8000SynMain: %s",
+	["open"] = "|c000070ddSynOpen: %s",
+	["gp"] = "|c00ffff00SynGP: %s",
 }
 
-local function strjoin(arr, sep)
-	local str = ''
-	local len = #arr
+local TYPE_CHAT_PREFIXES = {
+	["main"] = "SynMain: %s",
+	["open"] = "SynOpen: %s",
+	["gp"] = "SynGP: %s",
+}
 
-	for i, s in ipairs(arr) do
-		str = str .. s
-		if i ~= len then
-			str = str .. sep
-		end
+function syn_create_loot_lines(itemlink)
+	local iid = itemlink:match("item:(%d+):")
+	local item = syn_find_loot(iid)
+	-- print(strconcat('scll ', iid, ": ", itemlink))
+	if not item then
+		return {}
 	end
-	return str
-end
 
-local function append_tooltip(tooltip)
-	local itemname, itemlink = tooltip:GetItem()
 	local lines = {}
-
-	if not itemlink then
-		return
-	end
 	
 	for i, row in ipairs(LOOT_ROLES) do
 		local key = row[1]
 		local fmt = row[2]
 		local line = row[3]
-		local item = syn_find_loot(itemlink:match("item:(%d+):"))
 		local itemstring = item and item[key]
 		if itemstring and itemstring ~= "" then
 			fmt = string.format(fmt, itemstring)
@@ -49,12 +46,76 @@ local function append_tooltip(tooltip)
 		end
 	end
 
+	local ret = {}
+
 	for k, arr in pairs(lines) do
-		local str = strjoin(arr, ', ')
-		local fmt = string.format(TYPE_PREFIXES[k] or k .. ': %s', str)
+		local str = syn_strjoin(arr, ', ')
+		-- local fmt = string.format(TYPE_TOOLTIP_PREFIXES[k] or k .. ': %s', str)
+		ret[k] = str
+	end
+
+	return ret
+end
+
+function syn_append_tooltip(tooltip)
+	local itemname, itemlink = tooltip:GetItem()
+
+	local lines = syn_create_loot_lines(itemlink)
+
+	for k, str in pairs(lines) do
+		local fmt = string.format(TYPE_TOOLTIP_PREFIXES[k] or k .. ': %s', str)
 		tooltip:AddLine(fmt)
 	end
 end
 
-GameTooltip:HookScript("OnTooltipSetItem", append_tooltip)
-ItemRefTooltip:HookScript("OnTooltipSetItem", append_tooltip)
+GameTooltip:HookScript("OnTooltipSetItem", syn_append_tooltip)
+ItemRefTooltip:HookScript("OnTooltipSetItem", syn_append_tooltip)
+
+local SYN_PRINT_TYPE = 'main'
+
+function syn_printloot(link)
+	if ( not link ) then
+		return false;
+	end
+	local lines = syn_create_loot_lines(link)
+	local msg = string.format(TYPE_CHAT_PREFIXES[SYN_PRINT_TYPE], lines[SYN_PRINT_TYPE] or 'none')
+	msg = syn_strconcat(link, " ", msg)
+	if ( ChatEdit_InsertLink(msg) ) then
+	elseif ( SocialPostFrame and Social_IsShown() and Social_InsertLink(msg) ) then
+	else
+		syn_SendMessageGroup(msg)
+	end
+	return true
+end
+
+local orig_hmic = HandleModifiedItemClick
+function syn_itemclick(link)
+	-- FrameXML/ItemButtonTemplate.lua
+	if IsAltKeyDown() then
+		return syn_printloot(link)
+	end
+	-- if ( IsModifiedClick("DRESSUP") ) then
+	-- 	return DressUpItemLink(link) or DressUpBattlePetLink(link) or DressUpMountLink(link)
+	-- end
+	return orig_hmic(link);
+end
+
+HandleModifiedItemClick = syn_itemclick
+
+SLASH_SYNLOOT1 = "/syn"
+SLASH_SYNLOOT2 = "/slp"
+
+SlashCmdList.SYNLOOT = function(input)
+    -- input = string.lower(input)
+    local bits = syn_strtok(input:lower(), ' ')
+    if bits[1] == 'main' then
+        SYN_PRINT_TYPE = 'main'
+    elseif bits[1] == 'open' then
+        SYN_PRINT_TYPE = 'open'
+    elseif bits[1] == 'gp' then
+        SYN_PRINT_TYPE = 'gp'
+    else
+		print(syn_strconcat('Use "/syn (main|open|gp)" to switch modes'))
+    end
+	print(syn_strconcat('Syndicate Loot Priority: in "', SYN_PRINT_TYPE, '" printing mode'))
+end
--- main_functions.lua
+++ main_functions.lua
@@ -1,13 +1,64 @@
 -- searches for a specific item's information in loot_table.lua
-loot_by_id = {}
+local syn_loot_by_id = {}
 
 do
     for index, value in next, inception_loot_table do
-        loot_by_id[value["id"]] = value
+        syn_loot_by_id[value["id"]] = value
     end
 end
 -- {["loot_zone"] = "Blackwing Lair", ["loot_bosses"] = "Razorgore the Untamed", ["loot_name"] = "Mantle of the Blackwing Cabal", ["loot_id"] = "19370", ["mainspec_1"] = "Caster DPS", ["mainspec_2"] = "Priest", ["offset"] = "", ["prio"] = ""},
 
 function syn_find_loot(item_id)
-    return loot_by_id[item_id]
+    return syn_loot_by_id[item_id]
+end
+
+function syn_strjoin(arr, sep)
+    local str = ''
+    local len = #arr
+
+    for i, s in ipairs(arr) do
+        str = str .. s
+        if i ~= len then
+            str = str .. sep
+        end
+    end
+    return str
+end
+
+function syn_strtok(word, ch)
+  local toks = {}
+  local i, j = 0, 0
+  while j ~= nil do
+    local start = j and j + 1 or 0
+    i, j = word:find(ch, start)
+    if i == nil then
+      i = #word + 1
+    end
+    local str = word:sub(start, i - 1)
+    table.insert(toks, str)
+    -- print(str .. ',' .. tostring(i) .. ',' .. tostring(j))
+  end
+  return toks
+end
+
+function syn_strconcat(...)
+    local str = ''
+    local args = {...}
+    for i = 1, #args do
+        str = str .. tostring(args[i])
+    end
+    return str
+end
+
+function syn_SendMessageGroup(...)
+    local words = strconcat(...)
+    -- words = "Big" words
+    if IsInRaid() then
+        SendChatMessage(words, "RAID")
+    elseif IsInGroup() then
+        SendChatMessage(words, "PARTY")
+    else--for solo raid
+    --     SendChatMessage(words, "SAY")
+        print(words)
+    end
 end
